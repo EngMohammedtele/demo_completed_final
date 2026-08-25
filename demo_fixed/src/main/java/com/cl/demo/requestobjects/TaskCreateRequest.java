5a7fc8153117494b2082e5270c2d7dd12ebfc6c6
mohammed said <mohed.said77@gmail.com>
2026-08-25 15:09:25 +0400

    private for tirle , desription , duedate, start date, and task status

--- demo_fixed/src/main/java/com/cl/demo/requestobjects/TaskCreateRequest.java
+++ demo_fixed/src/main/java/com/cl/demo/requestobjects/TaskCreateRequest.java
@@ -9,3 +9,10 @@ import java.util.Date;
 @Getter
 @Setter
 public class TaskCreateRequest {
+    private String title;
+    private String description;
+    private Date dueDate;
+    private Date startDate;
+    private TaskStatus taskStatus;
+    private Boolean isAssigned;
+}
