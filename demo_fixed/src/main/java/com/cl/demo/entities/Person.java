3c443311845a77ba681f259e932c67729664ad5a
mohammed said <mohed.said77@gmail.com>
2026-08-25 14:57:09 +0400

    use getter and setter

--- demo_fixed/src/main/java/com/cl/demo/entities/Person.java
+++ demo_fixed/src/main/java/com/cl/demo/entities/Person.java
@@ -8,10 +8,3 @@ import java.util.List;
 @Setter
 @Getter
 public class Person extends BaseClass {
-    private String name;
-    private UserName userName;
-    private String email;
-    private PhoneNumber phoneNumber;
-    private List<Task> tasks;
-
-}
