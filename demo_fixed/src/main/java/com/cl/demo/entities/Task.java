1da7f8a6db8dd1d85bfbaae57763bd6f2687e7b1
mohammed said <mohed.said77@gmail.com>
2026-08-25 14:59:30 +0400

    use getter and setter for task

--- demo_fixed/src/main/java/com/cl/demo/entities/Task.java
+++ demo_fixed/src/main/java/com/cl/demo/entities/Task.java
@@ -8,13 +8,3 @@ import java.util.Date;
 @Setter
 @Getter
 public class Task extends BaseClass {
-    private String title;
-    private String description;
-    private String taskNumber;
-    private Date dueDate;
-    private Date startDate;
-    private Date endDate;
-    private TaskStatus taskStatus;
-    private Boolean isAssigned;
-
-}
