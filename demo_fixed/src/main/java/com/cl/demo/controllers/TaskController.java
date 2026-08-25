d72a30a6574dfb86daa9ef086d7dfdb640ce0c85
mohammed said <mohed.said77@gmail.com>
2026-08-25 14:52:14 +0400

    call requrst mapping and restcontroller

--- demo_fixed/src/main/java/com/cl/demo/controllers/TaskController.java
+++ demo_fixed/src/main/java/com/cl/demo/controllers/TaskController.java
@@ -14,21 +14,3 @@ import java.util.Map;
 @RestController
 @RequestMapping("task")
 public class TaskController {
-    @Autowired
-    private TaskService taskService;
-
-    @PostMapping("add")
-    public Map<String, String> addTask(@RequestBody TaskCreateRequest request) { return taskService.addTask(request); }
-
-    @GetMapping("getById")
-    public TaskCreateResponse getTaskById(@RequestParam String uuid) { return TaskCreateResponse.convert(taskService.getTaskById(uuid)); }
-
-    @GetMapping("getAll")
-    public List<TaskCreateResponse> getAllTasks() { return TaskCreateResponse.convert(taskService.getAllTasks()); }
-
-    @PutMapping("update")
-    public TaskUpdateResponse updateTask(@RequestBody TaskUpdateRequest request) { return TaskUpdateResponse.convert(taskService.updateTask(request)); }
-
-    @DeleteMapping("deleteById")
-    public Boolean deleteTaskById(@RequestParam String id) { return taskService.deleteById(id); }
-}
