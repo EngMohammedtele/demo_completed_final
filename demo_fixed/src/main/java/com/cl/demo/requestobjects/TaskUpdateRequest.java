655a0bf64ad3897a5ffc549781c84c56b4bca1e0
mohammed said <mohed.said77@gmail.com>
2026-08-25 15:10:02 +0400

    use getter and setterfor  TaskUpdateRequest

--- demo_fixed/src/main/java/com/cl/demo/requestobjects/TaskUpdateRequest.java
+++ demo_fixed/src/main/java/com/cl/demo/requestobjects/TaskUpdateRequest.java
@@ -9,12 +9,3 @@ import java.util.Date;
 @Getter
 @Setter
 public class TaskUpdateRequest {
-    private String uuid;
-    private String titleToUpdate;
-    private String descriptionToUpdate;
-    private Date dueDateToUpdate;
-    private Date startDateToUpdate;
-    private Date endDateToUpdate;
-    private TaskStatus taskStatusToUpdate;
-    private Boolean isAssignedToUpdate;
-}
