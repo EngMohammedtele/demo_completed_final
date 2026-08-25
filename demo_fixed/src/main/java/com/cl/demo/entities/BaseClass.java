273a6200046b7868a42829d52fa1a38dafbb530d
mohammed said <mohed.said77@gmail.com>
2026-08-25 14:55:22 +0400

    use getter and setter

--- demo_fixed/src/main/java/com/cl/demo/entities/BaseClass.java
+++ demo_fixed/src/main/java/com/cl/demo/entities/BaseClass.java
@@ -8,10 +8,3 @@ import java.util.UUID;
 
 @Getter
 @Setter
-public class BaseClass {
-
-    private UUID id;
-    private Boolean isActive;
-    private Date createdDate;
-    private Date updatedDate;
-}
